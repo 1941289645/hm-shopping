ba912c839705b8c58dbf3c17ef54299dd59b7681
LHC <124650324+1941289645@users.noreply.github.com>
2023-08-27 17:56:47 +0800

    postcess组件进行vw适配

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <div class="box"></div>
     <van-button type="primary">主要按钮</van-button>
 
     <router-view/>
@@ -7,5 +8,9 @@
 </template>
 
 <style lang="less">
-
+.box {
+  width: 300px;
+  height: 300px;
+  background-color: red;
+}
 </style>
