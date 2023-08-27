b0505c058e2b918cb2306955588d4c3968d797b5
LHC <124650324+1941289645@users.noreply.github.com>
2023-08-28 00:10:11 +0800

    登录静态页面布局
    request模块-axios封装，图形验证码功能

--- src/utils/vant-ui.js
+++ src/utils/vant-ui.js
@@ -4,7 +4,8 @@ import Vue from 'vue'
 // import 'vant/lib/index.css'
 // // 把vant中所有的组件都导入了
 // Vue.use(Vant)
-import { Button, Tabbar, TabbarItem } from 'vant'
+import { Button, Tabbar, TabbarItem, NavBar } from 'vant'
 Vue.use(Button)
 Vue.use(Tabbar)
 Vue.use(TabbarItem)
+Vue.use(NavBar)
