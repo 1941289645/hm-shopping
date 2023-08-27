cdd79a646975864fb281062c45a446fc1039da8c
LHC <124650324+1941289645@users.noreply.github.com>
2023-08-27 12:00:29 +0800

    vant组件库适配

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <van-button type="primary">主要按钮</van-button>
+
     <router-view/>
   </div>
 </template>
---
+++ src/utils/vant-ui.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+// 全部导入
+// import Vant from 'vant'
+// import 'vant/lib/index.css'
+// // 把vant中所有的组件都导入了
+// Vue.use(Vant)
+import { Button } from 'vant'
+Vue.use(Button)
